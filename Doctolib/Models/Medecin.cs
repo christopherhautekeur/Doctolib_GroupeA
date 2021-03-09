fbd82f6fe1073427d3ae5eaaf521ba70b472065c
Hautekeur Christopher <christopher.hautekeur@gmail.com>
2021-03-09 15:51:35 +0100

    Gestion bdd medecin

--- Doctolib/Models/Medecin.cs
+++ Doctolib/Models/Medecin.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Doctolib.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Doctolib.Models
@@ -13,10 +15,166 @@ namespace Doctolib.Models
         private DateTime embauche;
         private string specialite;
 
+        private static string request;
+        private static SqlCommand command;
+        private static SqlDataReader reader;
+
+        public Medecin()
+        {
+            Rdvs = new List<RDV>();
+        }
+
         public int Code { get => code; set => code = value; }
         public string Nom { get => nom; set => nom = value; }
         public string Telephone { get => telephone; set => telephone = value; }
         public DateTime Embauche { get => embauche; set => embauche = value; }
         public string Specialite { get => specialite; set => specialite = value; }
+
+        #region BDD
+        public bool Save()
+        {
+            request = "INSERT INTO Medecin (NomMedecin, TelMedecin, DateEmbauche, SpecialiteMedecin) OUTPUT INSERTED.CodeMedecin VALUES " +
+                "(@nom, @tel, @embauche, @spe)";
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@nom", System.Data.SqlDbType.VarChar) { Value = Nom });
+            command.Parameters.Add(new SqlParameter("@tel", System.Data.SqlDbType.VarChar) { Value = Telephone });
+            command.Parameters.Add(new SqlParameter("@embauche", System.Data.SqlDbType.Date) { Value = Embauche });
+            command.Parameters.Add(new SqlParameter("@spe", System.Data.SqlDbType.VarChar) { Value = Specialite });
+
+            DataBase.Connection.Open();
+            Code = (int)command.ExecuteScalar();
+
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return Code > 0;
+        }
+        public bool Update()
+        {
+            request = "UPDATE Medecin SET NomMedecin = @nom, TelMedecin = @tel, DateEmbauche = @emabauche, SpecialiteMedecin = @spe WHERE CodeMedecin = @code";
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@nom", System.Data.SqlDbType.VarChar) { Value = Nom });
+            command.Parameters.Add(new SqlParameter("@tel", System.Data.SqlDbType.VarChar) { Value = Telephone });
+            command.Parameters.Add(new SqlParameter("@embauche", System.Data.SqlDbType.Date) { Value = Embauche });
+            command.Parameters.Add(new SqlParameter("@spe", System.Data.SqlDbType.VarChar) { Value = Specialite });
+            command.Parameters.Add(new SqlParameter("@code", System.Data.SqlDbType.Int) { Value = Code });
+
+            DataBase.Connection.Open();
+            int row = command.ExecuteNonQuery();
+
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return row > 0;
+        }
+        public bool Delete()
+        {
+            request = "DELETE FROM Medecin WHERE CodeMedecin = @code";
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@code", System.Data.SqlDbType.Int) { Value = Code });
+
+            DataBase.Connection.Open();
+            int row = command.ExecuteNonQuery();
+
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return row > 0;
+        }
+        public static List<Medecin> GetListeMedecins()
+        {
+            List<Medecin> medecins = new List<Medecin>();
+
+            // LEFT JOIN pour recuperer la liste des rdv en meme temps que le medecin (moins de requete)
+            request = "SELECT m.CodeMedecin, m.NomMedecin, m.TelMedecin, m.DateEmbauche, m.SpecialiteMedecin, r.NumeroRDV, r.DateRDV, r.HeureRDV, r.CodePatient "+
+                "FROM Medecin m LEFT JOIN RDV r ON m.CodeMedecin = r.CodeMedecin";
+            command = new SqlCommand(request, DataBase.Connection);
+
+            DataBase.Connection.Open();
+            reader = command.ExecuteReader();
+
+            Medecin medecin = new Medecin();
+
+            while (reader.Read())
+            {
+                medecin = new Medecin();
+                medecin.Code = reader.GetInt32(0);
+                medecin.Nom = reader.GetString(1);
+                medecin.Telephone = reader.GetString(2);
+                medecin.Embauche = Convert.ToDateTime(reader.GetString(3));
+                medecin.Specialite = reader.GetString(4);
+
+                medecins.Add(medecin);
+            }
+            
+
+            reader.Close();
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return medecins;
+        }
+        public static List<Medecin> GetListeMedecinsBySpecialite(string specialite)
+        {
+            List<Medecin> medecins = new List<Medecin>();
+
+            request = "SELECTm.CodeMedecin, m.NomMedecin, m.TelMedecin, m.DateEmbauche, m.SpecialiteMedecin, r.NumeroRDV, r.DateRDV, r.HeureRDV, r.CodePatient " 
+                + "FROM Medecin m LEFT JOIN RDV r ON m.CodeMedecin = r.CodeMedecin"
+                + "WHERE m.SpecialiteMedecin like @spe";
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@spe", System.Data.SqlDbType.VarChar) { Value = specialite });
+
+            DataBase.Connection.Open();
+            reader = command.ExecuteReader();
+
+            Medecin medecin = new Medecin();
+
+            while (reader.Read())
+            {
+                medecin = new Medecin();
+                medecin.Code = reader.GetInt32(0);
+                medecin.Nom = reader.GetString(1);
+                medecin.Telephone = reader.GetString(2);
+                medecin.Embauche = Convert.ToDateTime(reader.GetString(3));
+                medecin.Specialite = reader.GetString(4);
+
+                medecins.Add(medecin);
+            }
+
+            reader.Close();
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return medecins;
+        }
+        public static Medecin GetMedecinByCode(int code)
+        {
+            Medecin medecin = null;
+
+            request = "SELECT CodeMedecin, NomMedecin, TelMedecin, DateEmbauche, SpecialiteMedecin"
+                + "WHERE CodeMedecin = @code";
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@code", System.Data.SqlDbType.Int) { Value = code });
+
+            DataBase.Connection.Open();
+            reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                medecin = new Medecin();
+                medecin.Code = reader.GetInt32(0);
+                medecin.Nom = reader.GetString(1);
+                medecin.Telephone = reader.GetString(2);
+                medecin.Embauche = Convert.ToDateTime(reader.GetString(3));
+                medecin.Specialite = reader.GetString(4);
+            }
+
+            reader.Close();
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return medecin;
+        }
+        #endregion
     }
 }
--- Doctolib/Models/RDV.cs
+++ Doctolib/Models/RDV.cs
@@ -4,19 +4,36 @@ using System.Text;
 
 namespace Doctolib.Models
 {
-    class RDV : AbstractModelWithNotification
+    public class RDV : AbstractModelWithNotification
     {
 
         private int numero;
         private DateTime date;
         private string heure;
-        private Medecin medecin;
-        private Patient patient;
+        private int codeMedecin;
+        private int codePatient;
 
         public int Numero { get => numero; set => numero = value; }
         public DateTime Date { get => date; set => date = value; }
         public string Heure { get => heure; set => heure = value; }
-        public Medecin Medecin { get => medecin; set => medecin = value; }
-        public Patient Patient { get => patient; set => patient = value; }
+        public int CodeMedecin { get => codeMedecin; set => codeMedecin = value; }
+        public int CodePatient { get => codePatient; set => codePatient = value; }
+
+        
+
+        public static List<RDV> GetRdvsByDate(DateTime date)
+        {
+            return null;
+        }
+
+        public static List<RDV> GetRdvsByCodePatient(int codePatient)
+        {
+            return null;
+        }
+
+        public static List<RDV> GetRdvs()
+        {
+            return null;
+        }
     }
 }
