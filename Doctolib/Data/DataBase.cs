074ec6d410ae1f197b0e731b7b24b9e63343cae4
Hautekeur Christopher <christopher.hautekeur@gmail.com>
2021-03-09 11:56:03 +0100

    Setup class base

---
+++ Doctolib/Data/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Doctolib.Data
+{
+    class DataBase
+    {
+        private static string chaine = @"Data Source=(LocalDB)\M2iProjet;Integrated Security=True";
+        public static SqlConnection Connection = new SqlConnection(chaine);
+    }
+}
