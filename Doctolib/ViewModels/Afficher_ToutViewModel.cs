a42ece6e583ad3a60863edcd16b286762fe81751
Hautekeur Christopher <christopher.hautekeur@gmail.com>
2021-03-17 17:14:21 +0100

    Modification page 'Afficher_Tout'

--- Doctolib/ViewModels/Afficher_ToutViewModel.cs
+++ Doctolib/ViewModels/Afficher_ToutViewModel.cs
@@ -14,10 +14,34 @@ namespace Doctolib.ViewModels
     class Afficher_ToutViewModel : ViewModelBase
     {
         
+        public bool radioBtnMedecins { get; set; }
+        public bool radioBtnPatients { get; set; }
+        public bool radioBtnRdvs { get; set; }
+        public ObservableCollection<Medecin> ListeMedecins { get; set; }
+        public ObservableCollection<Patient> ListePatients { get; set; }
+        public ObservableCollection<RDV> ListeRdvs { get; set; }
+        public Visibility ListeMedecinsVisibility { get { return radioBtnMedecins ? Visibility.Visible : Visibility.Collapsed; }}
+        public Visibility ListePatientsVisibility { get { return radioBtnPatients ? Visibility.Visible : Visibility.Collapsed; } }
+        public Visibility ListeRdvsVisibility { get { return radioBtnRdvs ? Visibility.Visible : Visibility.Collapsed; } }
+        public ICommand RadioBtnCommand { get; set; }
+
         public Afficher_ToutViewModel() {
-            Liste = new ObservableCollection<RDV>(RDV.GetAll());
+            ListeMedecins = new ObservableCollection<Medecin>(Medecin.GetListeMedecins());
+            ListePatients = new ObservableCollection<Patient>(Patient.GetAll());
+            ListeRdvs = new ObservableCollection<RDV>(RDV.GetAll());
+            RadioBtnCommand = new RelayCommand(ActionRadioBtnChanged);
         }
 
-        public ObservableCollection<RDV> Liste { get; set; }
+        public void ActionRadioBtnChanged()
+        {
+            RaiseAllChanged();
+        }
+
+        private void RaiseAllChanged()
+        {
+            RaisePropertyChanged("ListeMedecinsVisibility");
+            RaisePropertyChanged("ListePatientsVisibility");
+            RaisePropertyChanged("ListeRdvsVisibility");
+        }
     }
 }
